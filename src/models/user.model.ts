49c144c669ddb9296bccd491c326915d0d4a22d8
PaulShpilsher <paul.shpilsher@gmail.com>
2019-10-25 14:27:25 -0400

    removed commented out code

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -35,7 +35,7 @@ const userSchema = new Schema({
         type: String,
         enum: roles,
         default: 'user'
-    },
+    }
     // tokens: [{
     //     token: {
     //         type: String,
@@ -66,56 +66,5 @@ userSchema.pre<UserDocument>('save', async function(next: NextFunction) {
         return next(error);
     }
 });
-
-// userSchema.statics = {
-//     roles,
-
-//     async get(id: number): Promise<UserDocument> {
-//         if (Types.ObjectId.isValid(id)) {
-//             const user: UserDocument = await this.findById(id).exec();
-//             if(existy(user)) {
-//                 return user;
-//             }
-//         }
-//         throw new ApiError({
-//             message: 'User does not exist',
-//             status: httpStatus.NOT_FOUND
-//         });
-//     },
-
-//     async findAndCheckPassword(email: string, password: string): Promise<{user: UserDocument, token: string}> {
-//         if (!existy(email) || !existy(password)) {
-//             throw new ApiError({
-//                 message: 'An email and password are required to generate a token',
-//                 status: httpStatus.BAD_REQUEST});
-//         }
-
-//         const user = await this.findOne({ email }).exec();
-//         if(existy(user) && (await bcryptCompare(user.password, password))) {
-//             return {
-//                 user,
-//                 token: user.token()
-//             };
-//         }
-//         throw new ApiError({
-//             message: 'Incorrect email or password',
-//             status: httpStatus.UNAUTHORIZED,
-//         });
-//     }
-//};
-
-// userSchema.statics.findByCredentials = async (email: string, password: string) => {
-//     // Search for a user by email and password.
-//     const user = await User.findOne({ email});
-//     if (!user) {
-//         throw new Error('Invalid login credentials');
-//     }
-
-//     const isPasswordMatch = await bcrypt.compare(password, user.password);
-//     if (!isPasswordMatch) {
-//         throw new Error('Invalid login credentials');
-//     }
-//     return user;
-// };
 
 export const UserModel: Model<UserDocument> = model<UserDocument>('User', userSchema);
