2aedaba5aedd429811cdb72e4982cddde9f0c688
PaulShpilsher <paul.shpilsher@gmail.com>
2019-10-27 17:02:30 -0400

    refactor and gen token when creating user

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,30 +1,39 @@
 import { Logger, getLogger } from 'log4js';
 import { Request, Response, NextFunction } from 'express';
 import { UserModel, UserDocument } from '../models/user.model';
-import { sendError, throwArgumentValidationFailed } from '../utils';
-
-const logger: Logger = getLogger('UserController');
+import { sendError } from '../utils';
+import { makeUserToken } from '../middleware/auth-token';
+import { server } from '../server';
 
 export abstract class UserController {
-    static async registerUser(req: Request, res: Response, next: NextFunction): Promise<void> {
+    static async registerUser(req: Request, res: Response, next: NextFunction) {
         const { email, password, username, role } = req.body;
-        if(!(email && password && username)) {
-            throwArgumentValidationFailed();
+        if(!email) {
+            return sendError(res, httpStatus.BAD_REQUEST, 'Email required');
+        }
+        if(!password) {
+            return sendError(res, httpStatus.BAD_REQUEST, 'Username password');
+        }
+        if(!username) {
+            return sendError(res, httpStatus.BAD_REQUEST, 'Username required');
         }
 
         try {
-            const userModel: UserDocument = await new UserModel({
+            const user: UserDocument = await new UserModel({
                 email,
                 password,
                 username,
                 role
             }).save();
-            logger.info('Registered user', userModel);
+
+            const token = await makeUserToken(user);
+
+            server.logger.info('Registered user', user);
+            return res.status(httpStatus.CREATED).json({ data: token });
         }
         catch(err) {
-            logger.error(`Registered user failed.  Error: ${err}, Request body: ${JSON.stringify(req.body)}`);
-            sendError(res, httpStatus.BAD_REQUEST, 'Unable to register user', err);
+            server.logger.error('Registered user failed', err);
+            return sendError(res, httpStatus.BAD_REQUEST, 'Unable to register user', err);
         }
-        next();
     }
 }
