94418ce2baf1486d72e88718eca0e5c3047076ff
PaulShpilsher <paul.shpilsher@gmail.com>
2019-10-27 17:02:41 -0400

    refactor

--- src/controllers/health-check.controller.ts
+++ src/controllers/health-check.controller.ts
@@ -2,8 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 import { sendSuccess } from '../utils';
 
 export abstract class HealthCheckController {
-    static greeting(req: Request, res: Response, next: NextFunction): void {
-        sendSuccess(res, 'Greetings from Athena');
-        next();
+    static greeting(req: Request, res: Response, next: NextFunction) {
+        return sendSuccess(res, 'Greetings from Athena');
     }
 }
