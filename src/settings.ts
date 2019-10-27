325b97be3579bb4cbb23c57f8f1b95bc44fcfb9b
PaulShpilsher <paul.shpilsher@gmail.com>
2019-10-27 14:58:59 -0400

    add redis uri

--- src/settings.ts
+++ src/settings.ts
@@ -1,33 +1,35 @@
 import dotenv from 'dotenv';
 import log4js from 'log4js';
 
-const err = (missingVariable: string ) => { throw new Error(`Missing environment variable ${missingVariable}`); };
+const getEnvVar = (envVariable: string, defaultValue?: string): string => {
+    const value = process.env[envVariable];
+    if(!!value) {
+        return value;
+    }
+    else if(!!defaultValue) {
+        return defaultValue;
+    }
+    else {
+        throw new Error(`Missing environment variable ${envVariable}`);
+    }
+};
 
 // load config into env
 dotenv.config();
 
-export const env: string = process.env.NODE_ENV || 'production';
-export const port: number = Number.parseInt(process.env.PORT || '4300', 10);
+export const env: string = getEnvVar('NODE_ENV', 'production');
+export const port: number = Number.parseInt(getEnvVar('PORT', '4300'), 10);
 
-if(!process.env.BCRYPT_SALT) {
-    err('BCRYPT_SALT');
-}
-export const bcryptSalt: string = process.env.BCRYPT_SALT as string;
+export const bcryptSalt: string = getEnvVar('BCRYPT_SALT');
 
-if(!process.env.JWT_SECRET) {
-    err('JWT_SECRET');
-}
-export const jwtSecret: string = process.env.JWT_SECRET as string;
-export const jwtExpirationSeconds: number = Number.parseInt(process.env.JWT_EXPIRATION_MINUTES || '20', 10) * 60;
-
+export const jwtSecret: string = getEnvVar('JWT_SECRET');
+export const jwtExpirationSeconds: number = Number.parseInt(getEnvVar('JWT_EXPIRATION_MINUTES', '5'), 10) * 60;
 export const uploadLimitMb = 5;
 
-if(!process.env.MONGO_URI) {
-    err('MONGO_URI');
-}
-export const mongoUri: string = process.env.MONGO_URI as string;
+export const mongoUri: string = getEnvVar('MONGO_URI');
+export const redisUri = getEnvVar('REDIS_URI');
 
-export const logLevel: string = process.env.LOG_LEVEL || 'info';
+export const logLevel: string = getEnvVar('LOG_LEVEL', 'info');
 log4js.configure({
     appenders: {
         console: { type: 'console' }
